0e39a908ba29567234ffe01e0c6bda63efe1559f
mitermayer reis <mitermayer.reis@gmail.com>
2013-07-26 12:09:56 +1000

    refactored bootstrap file

--- core/js/monkeytestjs.js
+++ core/js/monkeytestjs.js
@@ -740,36 +740,30 @@
 /* globals QUnit, test, asyncTest */
 (function (global) {
 
-    // APP namespace
-    var APP = global._MonkeyTestJS = global._MonkeyTestJS || {};
-
     // block QUnit to try autostart without being ready
     global.QUnit.config.autostart = false;
 
-    // jquery no conflict 
-    var $$ = global.$$ = global.jQuery.noConflict(true);
+    var APP = global._MonkeyTestJS = global._MonkeyTestJS || {}, // APP namespace
+        $$ = global.$$ = global.jQuery.noConflict(true), // jquery no conflict 
+        monkeytestjs = global.monkeytestjs = new APP.MonkeyTestJS(), // create our singleton
+        START = function() {
+            // read configuration from a file called 'config.json'
+            $$.getJSON('config.json', function (data) {
 
-    // create our singleton / factory
-    var monkeytestjs = global.monkeytestjs = new APP.MonkeyTestJS();
+                monkeytestjs.start($$.extend({}, {
+                    workspace: window.frames[0],
+                    jQuery: $$
+                }, data));
+            })
+                .fail(function () {
+                    global.alert(
+                        "Failed to load config.json, please make sure this file exist and it is correctly formatted."
+                    );
+                });
 
-    // TODO: create a nicer method to wrap this startup
-    // start runner with json config file
-    $$(function () {
+        };
 
-        // read configuration from a file called 'config.json'
-        $$.getJSON('config.json', function (data) {
-
-            monkeytestjs.start($$.extend({}, {
-                workspace: window.frames[0],
-                jQuery: $$
-            }, data));
-        })
-            .fail(function () {
-                global.alert(
-                    "Failed to load config.json, please make sure this file exist and it is correctly formatted."
-                );
-            });
-
-    });
+    // When dom is ready read 'config.json' file and kickstart application.
+    $$(START);
 
 }(this));
