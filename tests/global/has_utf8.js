f1d45af9d872e8d2c2acb5e4b5d4576f72ff0e60
mitermayer reis <mitermayer.reis@gmail.com>
2013-07-29 13:31:28 +1000

    refactored utf8 test

--- tests/global/has_utf8.js
+++ tests/global/has_utf8.js
@@ -1,16 +1,16 @@
-registerTest ('Has a UTF-8 meta tag',
-    {
-        setup:function () {
+registerTest ('Has a UTF-8 meta tag', {
+    setup:function () {
 
-        },
-        load : function () {
-            this
-            .loadPageSource ()
-            .test("Do we have a UTF8 meta tag?", function() {
-                var pos = this.page.source.toLowerCase().indexOf('<meta charset="utf-8">');
-                notEqual(pos,-1,"Has UTF-8 meta tag");
-            })
-            .start();
-        }
+        // lets store the source code as lowercase in order to validate UTF-8 and utf-8
+        this.sourceCode = this.page.source.toLowerCase();
+    },
+    load : function () {
+        this
+        .loadPageSource() // making sure that the dom has finished loading on the iframe
+        .test("Do we have a UTF8 meta tag?", function() {
+            // do we have an utf8 metatag ?
+            notEqual(this.sourceCode.indexOf('<meta charset="utf-8">'),-1,"Has UTF-8 meta tag");
+        })
+        .start();
     }
-);
+});
