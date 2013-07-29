c828c54f3676794282f0a78e4fec391caf36a608
mitermayer reis <mitermayer.reis@gmail.com>
2013-07-29 13:57:43 +1000

    removed not_server_error test and refactored validator test

--- tests/global/html_validate.js
+++ tests/global/html_validate.js
@@ -1,36 +1,47 @@
-registerTest ('Has Valid HTML',
-    {
-        setup:function () {
+registerTest ('Has a Valid HTML', {
+    setup:function () {
+        this.proxyUrl = "/tests/core/proxy.php?url=";
+        this.validatorUrl = "validator.w3.org/check";
+        this.imagesFolder = "core/images/";
+    },
+    load : function () {
+        this
+        .loadPageSource() // wait page be loaded on iframe
+        .asyncTest("Is HTML Valid?",function() {
 
-        },
-        load : function () {
-            this
-            .loadPageSource ()
-            .asyncTest("Is HTML Valid?",function() {
-                var _this = this;
-                $$.post('/tests/core/proxy.php?url=validator.w3.org/check',{fragment:this.page.source})
-                .success(function(data) {
+            var self = this;
 
-                    var doc = $$(data.contents.replace(/images\//g, "core/images/"));
-                        
-                    var errors = doc.find('li.msg_err');
-                    if (errors.length>0)
-                    {
-                        errors.each(function(){
-                          var msg = $$(this).find('span.msg');
-                          ok (false, msg.text());
-                        });
-                    } else {
-                        ok( true, "HTML is valid" );
-                    }
-                    _this.asyncTestDone();
-                })
-                .error(function() {
-                    ok( false, "Unable to get validation results" );
-                    _this.asyncTestDone();
-                });
+            $$.post(this.proxyUrl + this.validatorUrl,{fragment:this.page.source})
+            .success(function(data) { // we got some validation results
+
+                // images are not in the root so lets change them to the correct path
+                var doc = $$(data.contents.replace(/images\//g, self.imagesFolder)),
+                    errors = doc.find('li.msg_err'); // error messages
+
+                if (errors.length) { // invalid page, use the validator messages for the errors.
+                    
+                    errors.each(function(){
+                      var msg = $$(this).find('span.msg');
+
+                      ok (false, msg.text());
+                    });
+                } else { // page is valid
+
+                    ok( true, "HTML is valid" );
+                }
+
+                // needs to be called upon assync tests
+                self.asyncTestDone();
             })
-            .start();
-        }
+            .error(function() { // validation couldnt be performed.
+
+                ok( false, "Unable to get validation results" );
+
+                // needs to be called upon assync tests
+                self.asyncTestDone();
+            });
+
+        })
+        .start();
     }
-);
+});
