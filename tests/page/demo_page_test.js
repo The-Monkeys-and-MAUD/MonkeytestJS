967e29c3bf0847bdd9671d049b56728ce8e791bd
Mitermayer Reis <mitermayer.reis@gmail.com>
2013-07-30 14:50:38 +1000

    new demo test

---
+++ tests/page/demo_page_test.js
@@ -0,0 +1,8 @@
+registerTest ('Form Validation test', function() {
+    this
+    .run(function(){
+    })
+    .test ("test we can submit the form",function($){
+        ok(true,'Form submits');
+    })
+});
