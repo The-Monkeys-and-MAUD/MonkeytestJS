e48dd153479ddbe163d027eca1404043d7f6bc7a
Pete Feltham <peter@feltham.com.au>
2014-02-03 14:29:04 +1100

    Fix error in is_html_w3c_valid.js global test when run with latest query

--- mytests/global/is_html_w3c_valid.js
+++ mytests/global/is_html_w3c_valid.js
@@ -18,7 +18,9 @@ registerTest ('Has a Valid HTML According To W3C Validator', {
 
                 this.post(this.validatorUrl, {fragment:this.page.source})
                 .success(function(data) { // we got some validation results
-
+                    if (typeof data === 'object' && data.contents) {
+                        data = data.contents;
+                    }
                     // images are not in the root so lets change them to the correct path
                     var doc = $$(data.replace(/images\//g, self.imagesFolder)),
                         errors = doc.find('li.msg_err'); // error messages
